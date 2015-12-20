573a7729559ee2c15e3e592f222324268a8320be
Cixtor <cixtords@gmail.com>
2015-12-20 09:00:07 -0500

    Added implementation for the auth.test method

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
 func main() {
 	var client SlackAPI
 
-	client.Test()
+	flag.Usage = func() {
+		fmt.Println("Usage:")
+		fmt.Println("  slackapi api.test")
+		fmt.Println("  slackapi auth.test")
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+	client.AutoConfigure()
+
+	switch flag.Arg(0) {
+	case "api.test":
+		client.Test()
+	case "auth.test":
+		client.AuthTest()
+	case "-help":
+		flag.Usage()
+	}
+
+	os.Exit(0)
 }
--- slackapi.go
+++ slackapi.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -8,15 +9,56 @@ import (
 )
 
 type SlackAPI struct {
+	Token string
+}
+
+func (s *SlackAPI) AutoConfigure() {
+	s.Token = os.Getenv("SLACK_TOKEN")
 }
 
 func (s *SlackAPI) ReportError(err error) {
-	fmt.Printf("Error: %s", err)
+	fmt.Printf("Error: %s\n", err)
 	os.Exit(1)
+}
+
+func (s *SlackAPI) PrintResponse(data []byte) {
+	var temp interface{}
+	err := json.Unmarshal(data, &temp)
+
+	if err != nil {
+		s.ReportError(err)
+	}
+
+	response, err := json.MarshalIndent(temp, "", "\x20\x20")
+
+	if err != nil {
+		s.ReportError(err)
+	}
+
+	fmt.Printf("%s\n", response)
+	os.Exit(0)
 }
 
 func (s *SlackAPI) GetRequest(action string, params ...string) []byte {
 	var url string = fmt.Sprintf("https://slack.com/api/%s", action)
+
+	if len(params) > 0 {
+		var anchor string
+
+		for key, keyvalue := range params {
+			if key == 0 {
+				anchor = "?"
+			} else {
+				anchor = "&"
+			}
+
+			if keyvalue == "token" {
+				keyvalue += "=" + s.Token
+			}
+
+			url += anchor + keyvalue
+		}
+	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	client := &http.Client{}
@@ -51,7 +93,10 @@ func (s *SlackAPI) GetRequest(action string, params ...string) []byte {
 
 func (s *SlackAPI) Test() {
 	response := s.GetRequest("api.test")
+	s.PrintResponse(response)
+}
 
-	fmt.Printf("%s\n", response)
-	os.Exit(0)
+func (s *SlackAPI) AuthTest() {
+	response := s.GetRequest("auth.test", "token")
+	s.PrintResponse(response)
 }
