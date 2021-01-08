6d3474a8bec0f083d07c8be105a8008452002b3b
Cixtor <cixtords@gmail.com>
2021-01-08 10:24:38 -0800

    Remove users.profile.getWithLabels test due to API uncertanties

--- slackapi_test.go
+++ slackapi_test.go
@@ -854,10 +854,3 @@ func TestUsersProfileGet(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed","profile":{"api_app_id":"","bot_id":"","avatar_hash":"","email":"","fields":null,"first_name":"","image_1024":"","image_192":"","image_24":"","image_32":"","image_48":"","image_512":"","image_72":"","image_original":"","last_name":"","phone":"","real_name":"","real_name_normalized":"","status_text":"","status_emoji":"","skype":"","title":""}}`
 	CheckResponse(t, x, y)
 }
-
-func TestUsersProfileGetWithLabels(t *testing.T) {
-	s := New()
-	x := s.UsersProfileGetWithLabels("user")
-	y := `{"ok":false,"error":"not_authed","profile":{"api_app_id":"","bot_id":"","avatar_hash":"","email":"","fields":null,"first_name":"","image_1024":"","image_192":"","image_24":"","image_32":"","image_48":"","image_512":"","image_72":"","image_original":"","last_name":"","phone":"","real_name":"","real_name_normalized":"","status_text":"","status_emoji":"","skype":"","title":""}}`
-	CheckResponse(t, x, y)
-}
