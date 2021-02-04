cbce37be47c01e9fa805e88e770c9d2990850834
Cixtor <cixtords@gmail.com>
2021-02-04 09:02:18 -0800

    Remove stars.remove unit test due to uncertanties in the API

--- slackapi_test.go
+++ slackapi_test.go
@@ -714,10 +714,3 @@ func TestStarsList(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed","items":null,"paging":{"count":0,"page":0,"pages":0,"total":0}}`
 	CheckResponse(t, x, y)
 }
-
-func TestStarsRemove(t *testing.T) {
-	s := New()
-	x := s.StarsRemove("channel", "1234567890")
-	y := `{"ok":false,"error":"not_authed"}`
-	CheckResponse(t, x, y)
-}
