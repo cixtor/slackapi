205510a15e359f95b6057a48f1da84d13baca536
Cixtor <cixtords@gmail.com>
2021-02-17 09:02:18 -0800

    Remove search.all unit test due to uncertanties in the API

--- slackapi_test.go
+++ slackapi_test.go
@@ -647,10 +647,3 @@ func TestResourceUnarchive(t *testing.T) {
 	y := `{"ok":false,"error":"unknown_method"}`
 	CheckResponse(t, x, y)
 }
-
-func TestSearchAll(t *testing.T) {
-	s := New()
-	x := s.SearchAll(SearchArgs{Query: "in:general"})
-	y := `{"ok":false,"error":"not_authed","query":"","files":{"matches":null,"pagination":{"first":0,"last":0,"page":0,"page_count":0,"per_page":0,"total_count":0},"paging":{"count":0,"page":0,"pages":0,"total":0},"total":0},"posts":{"matches":null,"pagination":{"first":0,"last":0,"page":0,"page_count":0,"per_page":0,"total_count":0},"paging":{"count":0,"page":0,"pages":0,"total":0},"total":0},"messages":{"matches":null,"pagination":{"first":0,"last":0,"page":0,"page_count":0,"per_page":0,"total_count":0},"paging":{"count":0,"page":0,"pages":0,"total":0},"total":0}}`
-	CheckResponse(t, x, y)
-}
