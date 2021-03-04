7719285d1c5cbcec22efd47155413340813766bb
Cixtor <cixtords@gmail.com>
2021-03-04 09:02:18 -0800

    Remove pins.removeFileComment unit test due to uncertanties in the API

--- slackapi_test.go
+++ slackapi_test.go
@@ -521,10 +521,3 @@ func TestPinsRemoveFile(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed"}`
 	CheckResponse(t, x, y)
 }
-
-func TestPinsRemoveFileComment(t *testing.T) {
-	s := New()
-	x := s.PinsRemove("channel", "Fc12345678")
-	y := `{"ok":false,"error":"not_authed"}`
-	CheckResponse(t, x, y)
-}
