74e677b1ef00aad56f91d6d5763be03d4ee31572
Cixtor <cixtords@gmail.com>
2021-04-01 14:25:39 -0800

    Remove files.comments.delete unit test due to API uncertanties

--- slackapi_test.go
+++ slackapi_test.go
@@ -262,10 +262,3 @@ func TestFilesCommentsAdd(t *testing.T) {
 	y := `{"ok":false,"error":"unknown_method","comment":{"comment":"","id":"","user":"","created":0,"timestamp":0,"is_intro":false}}`
 	CheckResponse(t, x, y)
 }
-
-func TestFilesCommentsDelete(t *testing.T) {
-	s := New()
-	x := s.FilesCommentsDelete("fileid", "commentid")
-	y := `{"ok":false,"error":"not_authed"}`
-	CheckResponse(t, x, y)
-}
