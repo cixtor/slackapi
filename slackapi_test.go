81957fc21542a225c582c102cb8c25c0b21b55ea
Cixtor <cixtords@gmail.com>
2021-04-02 09:02:18 -0800

    Remove files.comments.add unit test due to API uncertanties

--- slackapi_test.go
+++ slackapi_test.go
@@ -255,10 +255,3 @@ func TestEmojiList(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed","cache_ts":"","emoji":null}`
 	CheckResponse(t, x, y)
 }
-
-func TestFilesCommentsAdd(t *testing.T) {
-	s := New()
-	x := s.FilesCommentsAdd("fileid", "comment")
-	y := `{"ok":false,"error":"unknown_method","comment":{"comment":"","id":"","user":"","created":0,"timestamp":0,"is_intro":false}}`
-	CheckResponse(t, x, y)
-}
