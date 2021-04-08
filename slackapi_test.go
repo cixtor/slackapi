d2578e3c20ca1832361815bd163ba6d01d2438a4
Cixtor <cixtords@gmail.com>
2021-04-08 09:02:18 -0800

    Remove conversations.invite unit test due to API uncertanties

--- slackapi_test.go
+++ slackapi_test.go
@@ -150,10 +150,3 @@ func TestConversationsJoin(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed","channel":{"created":0,"creator":"","id":"","is_archived":false,"is_channel":false,"is_general":false,"is_group":false,"is_member":false,"is_mpim":false,"is_open":false,"last_read":"","latest":{"text":"","ts":"","type":"","user":""},"members":null,"name":"","name_normalized":"","num_members":0,"purpose":{"creator":"","last_set":0,"value":""},"topic":{"creator":"","last_set":0,"value":""},"unread_count":0,"unread_count_display":0}}`
 	CheckResponse(t, x, y)
 }
-
-func TestConversationsInvite(t *testing.T) {
-	s := New()
-	x := s.ConversationsInvite("channel", "user1", "user2", "user3")
-	y := `{"ok":false,"error":"not_authed","channel":{"created":0,"creator":"","id":"","is_archived":false,"is_channel":false,"is_general":false,"is_group":false,"is_member":false,"is_mpim":false,"is_open":false,"last_read":"","latest":{"text":"","ts":"","type":"","user":""},"members":null,"name":"","name_normalized":"","num_members":0,"purpose":{"creator":"","last_set":0,"value":""},"topic":{"creator":"","last_set":0,"value":""},"unread_count":0,"unread_count_display":0}}`
-	CheckResponse(t, x, y)
-}
