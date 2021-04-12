91db4175e8965291afbbbfaa7a24993f9f2569c2
Cixtor <cixtords@gmail.com>
2021-04-12 10:24:38 -0800

    Remove conversations.create unit test due to API uncertanties

--- slackapi_test.go
+++ slackapi_test.go
@@ -122,10 +122,3 @@ func TestConversationsArchive(t *testing.T) {
 	y := `{"ok":false,"error":"not_authed"}`
 	CheckResponse(t, x, y)
 }
-
-func TestConversationsCreate(t *testing.T) {
-	s := New()
-	x := s.ConversationsCreate("channel")
-	y := `{"ok":false,"error":"not_authed","channel":{"created":0,"creator":"","id":"","is_archived":false,"is_channel":false,"is_general":false,"is_group":false,"is_member":false,"is_mpim":false,"is_open":false,"last_read":"","latest":{"text":"","ts":"","type":"","user":""},"members":null,"name":"","name_normalized":"","num_members":0,"purpose":{"creator":"","last_set":0,"value":""},"topic":{"creator":"","last_set":0,"value":""},"unread_count":0,"unread_count_display":0}}`
-	CheckResponse(t, x, y)
-}
