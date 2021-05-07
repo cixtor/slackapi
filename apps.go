75f401fd8a59eb6ca32d2fb62ad01733677ffea5
Cixtor <cixtords@gmail.com>
2021-05-07 09:00:00 -0800

    Add implementation for the apps.event.authorizations.list method

--- apps.go
+++ apps.go
@@ -14,3 +14,30 @@ func (s *SlackAPI) AppsConnectionsOpen() AppsConnectionsOpenResponse {
 	}
 	return out
 }
+
+type AppsEventAuthorizationsListInput struct {
+	EventContext string `json:"event_context"`
+	Cursor       string `json:"cursor"`
+	Limit        int    `json:"limit"`
+}
+
+type AppsEventAuthorizationsListResponse struct {
+	Response
+	Authorizations []AppAuthorization `json:"authorizations"`
+}
+
+type AppAuthorization struct {
+	EnterpriseID string `json:"enterprise_id"`
+	TeamID       string `json:"team_id"`
+	UserID       string `json:"user_id"`
+	IsBot        string `json:"is_bot"`
+}
+
+// AppsEventAuthorizationsList is https://api.slack.com/methods/apps.event.authorizations.list
+func (s *SlackAPI) AppsEventAuthorizationsList(input AppsEventAuthorizationsListInput) AppsEventAuthorizationsListResponse {
+	var out AppsEventAuthorizationsListResponse
+	if err := s.basePOST("/api/apps.event.authorizations.list", input, &out); err != nil {
+		return AppsEventAuthorizationsListResponse{Response: Response{Error: err.Error()}}
+	}
+	return out
+}
