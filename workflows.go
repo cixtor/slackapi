4401c422181315dcfab7bb57aa9a0fa282662340
Cixtor <cixtords@gmail.com>
2021-06-10 09:00:00 -0800

    Add request parameters in the workflows.stepCompleted method

--- workflows.go
+++ workflows.go
@@ -1,9 +1,18 @@
 package slackapi
 
+type WorkflowsStepCompletedInput struct {
+	// Context identifier that maps to the correct workflow step execution.
+	WorkflowStepExecuteID string `json:"workflow_step_execute_id"`
+	// Key-value object of outputs from your step. Keys of this object
+	// reflect the configured key properties of your outputs array from
+	// your workflow_step object.
+	Outputs map[string]string `json:"outputs"`
+}
+
 // WorkflowsStepCompleted https://api.slack.com/methods/workflows.stepCompleted
-func (s *SlackAPI) WorkflowsStepCompleted() Response {
+func (s *SlackAPI) WorkflowsStepCompleted(input WorkflowsStepCompletedInput) Response {
 	var out Response
-	if err := s.basePOST("/api/workflows.stepCompleted", nil, &out); err != nil {
+	if err := s.basePOST("/api/workflows.stepCompleted", input, &out); err != nil {
 		return Response{Error: err.Error()}
 	}
 	return out
