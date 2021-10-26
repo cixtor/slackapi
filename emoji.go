2b221abefed5de615b47804cf7a50c837e833865
Cixtor <cixtords@gmail.com>
2021-10-26 10:34:28 -0800

    Add alternative emoji.list function to include categories

--- emoji.go
+++ emoji.go
@@ -1,5 +1,9 @@
 package slackapi
 
+import (
+	"net/url"
+)
+
 // Emoji contains the data associated to a single emoji.
 type Emoji struct {
 	AliasFor        string   `json:"alias_for"`
@@ -31,6 +35,29 @@ func (s *SlackAPI) EmojiList() EmojiListResponse {
 	var out EmojiListResponse
 	if err := s.baseGET("/api/emoji.list", nil, &out); err != nil {
 		return EmojiListResponse{Response: Response{Error: err.Error()}}
+	}
+	return out
+}
+
+type EmojiListWithCategoriesResponse struct {
+	EmojiListResponse
+	CategoriesVersion string          `json:"categories_version"`
+	Categories        []EmojiCategory `json:"categories"`
+}
+
+type EmojiCategory struct {
+	Name       string   `json:"name"`
+	EmojiNames []string `json:"emoji_names"`
+}
+
+// EmojiListWithCategories lists custom emoji for a team with categories.
+func (s *SlackAPI) EmojiListWithCategories() EmojiListWithCategoriesResponse {
+	in := url.Values{"include_categories": {"true"}}
+	var out EmojiListWithCategoriesResponse
+	if err := s.baseGET("/api/emoji.list", in, &out); err != nil {
+		out = EmojiListWithCategoriesResponse{}
+		out.Error = err.Error()
+		return out
 	}
 	return out
 }
